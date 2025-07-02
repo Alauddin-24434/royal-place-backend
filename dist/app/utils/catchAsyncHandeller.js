b6edbba646043d0bd67cc5c659d9d6a264970313
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-07-02 22:39:46 +0600

    Remove dist folder from repo and add to .gitignore

--- dist/app/utils/catchAsyncHandeller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsyncHandeller = void 0;
-const catchAsyncHandeller = (fn) => {
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    };
-};
-exports.catchAsyncHandeller = catchAsyncHandeller;
--- dist/app/utils/logger/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const config_1 = require("../../config");
-const developmentLogger_1 = require("./developmentLogger");
-const productionLogger_1 = require("./productionLogger");
-exports.logger = config_1.envVariable.ENV === "production" ? productionLogger_1.productionLogger : developmentLogger_1.developmentLogger;
