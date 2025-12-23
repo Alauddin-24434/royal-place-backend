f42aace221a8e0a12acd58ca3ec8756eef9113ed
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2025-12-23 10:40:53 +0600

    remove nested folder

---
+++ src/app/routes/payment.route.ts
@@ -0,0 +1,31 @@
+import express, { Router } from "express";
+
+import { paymentController } from "../controllers/payment.controllers";
+
+const router = express.Router();
+
+// payment sucess
+router.post(
+  "/success",
+
+  paymentController.paymentSuccess
+);
+// payment fail
+router.post(
+  "/fail",
+
+  paymentController.paymentFail
+);
+
+// payment fail
+router.get(
+  "/cancel",
+
+  paymentController.paymentCancel
+);
+// get all paymnets
+router.get("/", paymentController.getPayments);
+// get payments by User id
+router.get("/:id", paymentController.getPaymentsByUserId);
+
+export const paymentRoute = router;
